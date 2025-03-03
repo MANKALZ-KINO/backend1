66fc17b95a0fd89190f2898dacc6615ff19a3e7e
amaliekea <amje0001@stud.kea.dk>
2025-03-03 16:00:08 +0100

    added findbyname endpoint to moviesRestController

--- src/main/java/com/example/backend/controllers/MovieRestController.java
+++ src/main/java/com/example/backend/controllers/MovieRestController.java
@@ -4,6 +4,7 @@ import com.example.backend.model.Movie;
 import com.example.backend.repositories.IMovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,7 +25,9 @@ public class MovieRestController {
     public List<Movie> movies() {
         return iMovieRepository.findAll();
     }
-
+    @GetMapping("students/{name}") public List<Movie> allMoviesByName(@PathVariable String movieName) {
+        return iMovieRepository.findAllByName(movieName);
+    }
 
 
 }
--- src/main/java/com/example/backend/repositories/IMoviePlanRepository.java
+++ src/main/java/com/example/backend/repositories/IMoviePlanRepository.java
@@ -1,7 +1,10 @@
 package com.example.backend.repositories;
 
+import com.example.backend.model.Movie;
 import com.example.backend.model.MoviePlan;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface IMoviePlanRepository extends JpaRepository<MoviePlan,Long> {
 }
