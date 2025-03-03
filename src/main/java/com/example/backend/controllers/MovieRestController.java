8479b050977cde672882953517e1292ca9a6c60d
amaliekea <amje0001@stud.kea.dk>
2025-03-03 15:53:06 +0100

    getmovies endpoint

---
+++ src/main/java/com/example/backend/controllers/MovieRestController.java
@@ -0,0 +1,30 @@
+package com.example.backend.controllers;
+
+import com.example.backend.model.Movie;
+import com.example.backend.repositories.IMovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MovieRestController {
+
+    @Autowired
+    IMovieRepository iMovieRepository;
+
+    @GetMapping("/")
+    public String detteErRoden() {
+        return "Du er i roden";
+    }
+
+    //GET
+    @GetMapping("/movies")
+    public List<Movie> movies() {
+        return iMovieRepository.findAll();
+    }
+
+
+
+}
