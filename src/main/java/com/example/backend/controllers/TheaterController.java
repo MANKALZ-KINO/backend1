8d4981ce7f919a2cb9f9ffe1fa9fd3065719f730
“zuunun” <zuun0001@stud.kea.dk>
2025-03-04 14:57:07 +0100

    all theater

--- src/main/java/com/example/backend/controllers/TheaterController.java
+++ src/main/java/com/example/backend/controllers/TheaterController.java
@@ -1,11 +1,14 @@
 package com.example.backend.controllers;
 
+import com.example.backend.model.MoviePlan;
 import com.example.backend.model.Theater;
 import com.example.backend.repositories.ITheaterRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,6 +26,17 @@ public class TheaterController {
     @GetMapping("/all")
     public List<Theater> getAllTheaters() {
         return theaterRepository.findAll();
+    }
+
+    //GET
+    @GetMapping("/movieplans/{id}")
+    public List<Theater> theaterById(@PathVariable Long id) {
+        List<Theater> moviePlanForMovie = new ArrayList<>();
+        for (Theater t : theaterRepository.findAll())
+            if (t.getTheaterId() == id){
+                moviePlanForMovie.add(t);
+            }
+        return moviePlanForMovie;
     }
 
 
