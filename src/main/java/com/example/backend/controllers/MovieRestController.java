b22971771577323c5705b89e6ab069bec83c345f
amaliekea <amje0001@stud.kea.dk>
2025-03-04 11:08:04 +0100

    postmovie working!!

--- src/main/java/com/example/backend/controllers/MovieRestController.java
+++ src/main/java/com/example/backend/controllers/MovieRestController.java
@@ -2,15 +2,14 @@ package com.example.backend.controllers;
 
 import com.example.backend.model.Movie;
 import com.example.backend.model.MoviePlan;
+import com.example.backend.repositories.IMoviePlanRepository;
 import com.example.backend.repositories.IMovieRepository;
+import com.example.backend.repositories.ITicketRepository;
 import com.example.backend.service.MoviePlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,7 +21,13 @@ public class MovieRestController {
     IMovieRepository iMovieRepository;
 
     @Autowired
+    IMoviePlanRepository iMoviePlanRepository;
+
+    @Autowired
     MoviePlanService moviePlanService;
+
+    @Autowired
+    ITicketRepository iTicketRepository;
 
     @GetMapping("/")
     public String detteErRoden() {
@@ -37,29 +42,16 @@ public class MovieRestController {
     @GetMapping("/movies/{name}") public List<Movie> allMoviesByName(@PathVariable String name) {
         return iMovieRepository.findAllBymovieName(name);
     }
-    //DELETE
-    // DELETE Movie
-    @DeleteMapping("/movie/{id}")
-    public ResponseEntity<String> deleteMovie(@PathVariable Long id) {
-        Optional<Movie> movie = iMovieRepository.findById(id);
-
-        if (movie.isPresent()) {
-            // Hent MoviePlans for den givne Movie
-            List<MoviePlan> moviePlans = movie.get().getMoviePlans();
-
-            // Slet kun MoviePlans, hvis der er nogen
-            if (moviePlans != null && !moviePlans.isEmpty()) {
-                moviePlans.forEach(moviePlan -> moviePlanService.deleteMoviePlan(moviePlan.getMoviePlanId()));
-            }
-
-            // Slet filmen bagefter
-            iMovieRepository.deleteById(id);
-
-            return ResponseEntity.ok("Movie and related MoviePlans deleted successfully!");
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Movie not found and not deleted");
-        }
+    //POST
+    @PostMapping("/movie")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Movie postMovie(@RequestBody Movie movie) {
+        System.out.println("indsætter ny movie!!");
+        System.out.println(movie);
+        return iMovieRepository.save(movie); //opda
     }
+
+
 
 
 
