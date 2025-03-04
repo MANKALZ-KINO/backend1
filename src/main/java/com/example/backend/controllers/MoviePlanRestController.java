1edf51fd13f06c66e03a3985c033111bef51318e
amaliekea <amje0001@stud.kea.dk>
2025-03-04 12:14:40 +0100

    updatemovie endpoint working!

--- src/main/java/com/example/backend/controllers/MoviePlanRestController.java
+++ src/main/java/com/example/backend/controllers/MoviePlanRestController.java
@@ -1,14 +1,17 @@
 package com.example.backend.controllers;
-import com.example.backend.model.Movie;
 import com.example.backend.model.MoviePlan;
 import com.example.backend.repositories.IMoviePlanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class MoviePlanRestController {
@@ -16,14 +19,26 @@ public class MoviePlanRestController {
     @Autowired
     IMoviePlanRepository iMoviePlanRepository;
 
-    @GetMapping("/moviePlans{id}")
-    public List<MoviePlan> moviePlans(@PathVariable int id) {
+    //GET
+    @GetMapping("/movieplans/{id}")
+    public List<MoviePlan> moviePlansWithMovieId(@PathVariable Long id) {
         List<MoviePlan> moviePlanForMovie = new ArrayList<>();
         for (MoviePlan m : iMoviePlanRepository.findAll())
             if (m.getMovie().getMovieId() == id){
                 moviePlanForMovie.add(m);
             }
         return moviePlanForMovie;
+    }
+    //DELETE
+    @DeleteMapping("/movieplan/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable Long id){
+        Optional<MoviePlan> orgStudent = iMoviePlanRepository.findById(id);
+        if (orgStudent.isPresent()) {
+            iMoviePlanRepository.deleteById(id);
+            return ResponseEntity.ok("movieplan deleted");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("movieplan not found and could not be deleted");
+        }
     }
 
 
