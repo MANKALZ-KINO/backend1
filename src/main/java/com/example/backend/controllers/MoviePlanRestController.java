6f288be3c63f05b148435569ed9c64a68124df4d
amaliekea <amje0001@stud.kea.dk>
2025-03-07 15:17:01 +0100

    getallmovieplandates

--- src/main/java/com/example/backend/controllers/MoviePlanRestController.java
+++ src/main/java/com/example/backend/controllers/MoviePlanRestController.java
@@ -8,17 +8,14 @@ import com.example.backend.service.MoviePlanService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+@CrossOrigin(origins = "*")
 @RestController
 public class MoviePlanRestController {
 
